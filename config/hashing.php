3a474d3fec0e11e3d91e0d9deea44e7a774c9cb8
ellysse-cy <aleese100421@gmail.com>
2026-02-11 14:16:59 +0800

    Add files via upload

---
+++ config/hashing.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'driver' => 'argon2id',  // Change from 'bcrypt' to 'argon2id'
+
+    'bcrypt' => [             
+        'rounds' => env('BCRYPT_ROUNDS', 12),
+    ],
+
+    'argon' => [
+        'memory' => 65536,
+        'threads' => 1,
+        'time' => 4,
+    ],
+];
